a5a9f40df3cdb72ee8f9020adba71f7dba18826a
Alex <alex@alex-dev.me>
2023-12-17 07:28:53 +0000

    Preparing scheduler

--- main.py
+++ main.py
@@ -10,14 +10,29 @@ load_dotenv()
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 
-# get plants from json file
+# get plants info from json file
 with open('plants.json') as f:
-    plants = json.load(f)
-print(plants)
+    data = json.load(f)
+    plants = data['plants']
+
+with open('config.json') as f:
+    config = json.load(f)
+
+
+def get_plant_info_by_name(plant_name):
+    for plant in plants:
+        if plant['name'] == plant_name:
+            return plant
+
+
+def set_chat_id(chat_id):
+    config['chat_id'] = chat_id
+    with open('config.json', 'w') as f:
+        json.dump(config, f)
 
 
 def check_account(account):
-    print(account)
+    print(f'Checking account {account}')
     client = Client(
         account,
         api_id=api_id,
@@ -31,20 +46,15 @@ def check_account(account):
 def get_existing_sessions():
     sessions = [file.split('.')[0]
                 for file in os.listdir() if file.endswith('.session')]
+    print(f'Found {len(sessions)} existing sessions')
     return sessions
 
 
 def test_existing_sessions():
+    print('Testing existing sessions')
     sessions = get_existing_sessions()
     for session in sessions:
         check_account(session)
-
-
-def check_existing_accounts_json():
-    with open('accounts.json') as f:
-        accounts = json.load(f)
-    for account in accounts:
-        check_account(account)
 
 
 def create_new_session(account):
@@ -58,7 +68,7 @@ def create_new_session(account):
     client.stop()
 
 
-def init_bot():
+def create_accounts_prompt():
     print('How many accounts do you want to create?')
     num_accounts = int(input())
     accounts = []
@@ -70,3 +80,61 @@ def init_bot():
         with open('accounts.json', 'w') as f:
             json.dump(accounts, f)
     print(f'Created {num_accounts} accounts')
+
+
+def init_bot():
+    if get_existing_sessions():
+        print('Existing sessions found, do you want to create new ones?')
+        print('y/n')
+        choice = input()
+        if choice == 'y':
+            create_accounts_prompt()
+    else:
+        create_accounts_prompt()
+
+
+def set_schedule(account):
+    client = Client(
+        account,
+        api_id=api_id,
+        api_hash=api_hash
+    )
+    client.start()
+    username = client.get_me().username
+    client.stop()
+    print(
+        f'What plant do you want to schedule for {username}?')
+    for plant in plants:
+        print(">", plant['name'])
+    plant_name = input('Enter plant name: ')
+    plant_info = get_plant_info_by_name(plant_name)
+    print(plant_info)
+
+
+def schedule_accounts():
+    sessions = get_existing_sessions()
+    for session in sessions:
+        set_schedule(session)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--test', action='store_true',
+                        help='Test existing sessions')
+    parser.add_argument('-c', '--create', action='store_true',
+                        help='Create new sessions')
+    parser.add_argument('-s', '--schedule', action='store_true',
+                        help='Schedule a plant for an account')
+    args = parser.parse_args()
+    if args.test:
+        test_existing_sessions()
+    elif args.create:
+        init_bot()
+    elif args.schedule:
+        schedule_accounts()
+    else:
+        print('No arguments passed')
+
+
+if __name__ == '__main__':
+    main()
